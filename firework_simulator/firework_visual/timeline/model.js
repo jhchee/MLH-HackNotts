9a397fbe47ce75961887f83672c2601770e81578
Brendan Lum <38660687+spotted-metal@users.noreply.github.com>
2018-11-24 18:19:51 +0000

    Please kill me

--- firework_simulator/firework_visual/timeline/model.js
+++ firework_simulator/firework_visual/timeline/model.js
@@ -1,3 +1,43 @@
-timeline_model = function() {
-  // TODO
+/* timeline_model = {
+  fireworks: [],
+  panel: {
+    startTime: 0,
+    endTime: 15,
+    x: 25,
+    y: 40,
+    width: 550,
+    height: 135
+  }
+}; */
+
+let timeline_model = function() {
+  let fireworks = [
+    { time: 5,  color: 'red'    },
+    { time: 7,  color: 'blue'   },
+    { time: 17, color: 'green'  },
+    { time: 25, color: 'yellow' },
+    { time: 36, color: 'orange' }
+  ];
+
+  let panel = {
+    startTime: 0,
+    endTime: 30,
+
+    x: 25,
+    y: 40,
+    width: 550,
+    height: 135
+  };
+
+  function addFirework(time, color) {
+    let firework = {time: time, color: color};
+    fireworks.push(firework);
+    fireworks.sort((a, b) => a.time - b.time)
+  }
+
+  return {
+    fireworks: fireworks,
+    panel: panel,
+    addFirework: addFirework
+  }
 }();
