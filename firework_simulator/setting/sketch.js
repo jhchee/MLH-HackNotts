b3dbcfa9130896f2fe0b8f73f0542657a824ae01
Desmond Lin <lqjlqj1997@hotmail.com>
2018-11-24 23:38:25 +0000

    init setting for firework

---
+++ firework_simulator/setting/sketch.js
@@ -0,0 +1,68 @@
+var timeSlice = [];
+
+function setup() {
+  createCanvas(windowWidth/2, windowHeight/2, WEBGL);
+  colorMode(HSB);
+  gravity = createVector(0, 0.2, 0);
+  stroke(255);
+  strokeWeight(4);
+  background(0);
+  createEasyCam({ distance: 400 });
+}
+
+function draw() {
+  
+  
+  colorMode(RGB);
+  background(0, 0, 0, 255);
+
+  // plane
+  push();
+  noStroke();
+  fill(100);
+  translate(0, 400, 0);
+  rotateX(0.5 * PI);
+  plane(800, 800);
+  pop();
+
+  // platform of lauching firework
+  push();
+  noStroke();
+  fill(255);
+  translate(0, 400, 0);
+  rotateX(0.5 * PI);
+  plane(40, 40);
+  pop();
+
+  // box of restriction
+  push();
+  translate(0, 0, 0);
+  noFill();
+  stroke(255);
+  strokeWeight(1);
+  box(800, 800, 800);
+  pop();
+
+  
+}
+
+
+function display(time){
+  var i = 0;
+  var j = 0;
+
+  for (i=0;i<timeSlice.length;i++){
+    if(timeSlice[i].launched_time==time){
+      var firework = timeSlice[i].firework;
+      for(j=0;j<timeSlice[i].firework.length;j++){
+        push();
+          translate(firework[j].pos.x,firework[j].pos.y,firework[j].pos.z);
+          sphere(1);
+        pop();
+      }
+      break;
+    }
+  }
+
+
+}
